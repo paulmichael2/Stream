fd14ade5ff5b5eec57bd4b457bb6a040d509575a
Paul <paulmacarasig2@gmail.com>
2023-06-01 23:14:34 +0800

    first commit

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+export default {
+  //Site display name
+  SITE_TITLE: "PAULFLIX",
+
+  //Put your discord link here or null to disable
+  SITE_DISCORD: "https://discord.com",
+  
+  //True or false to show or hide site credit in footer (You should leave enabled 😉)
+  SITE_CREDITS: false,
+
+  //Do not change or site will break!
+  RIPPER_API: "https://api.ripper.fun"
+
+
+  
+}
